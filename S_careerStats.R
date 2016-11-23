389481ce5e1f81a4af58979560ea41e827090544
Gil Chen <wqchen68@gmail.com>
2016-11-24 00:56:39 +0800

    revised S_careerStats.R

--- S_careerStats.R
+++ S_careerStats.R
@@ -4,10 +4,11 @@ library(RMySQL)
 library(rvest)
 library(stringr)
 
-htmlstr <- read_html("http://sports.yahoo.com/nba/players/3331/")
+fbido <- '3331'
+htmlstr <- read_html(paste0("http://sports.yahoo.com/nba/players/", fbido))
 
 careerStats <- htmlstr %>%
   html_table(header=FALSE, fill=T) %>% .[[5]] %>% 
   filter(grepl("-", X1)) %>%
-  'colnames<-'(c('cseason','cteam','cgame','acmin','acfgm','acfga','ac3ptm','ac3pta','acftm','acfta','acoreb','acdreb','actreb','acast','acto','acst','acblk','acpf','acpts'))
-  
+  'colnames<-'(c('cseason','cteam','cgame','acmin','acfgm','acfga','ac3ptm','ac3pta','acftm','acfta','acoreb','acdreb','actreb','acast','acto','acst','acblk','acpf','acpts')) %>%
+  mutate(fbido=fbido)
