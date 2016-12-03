b5a47511fd4a40fc207e81bd0ef54c75847d31ba
Gil Chen <wqchen68@gmail.com>
2016-12-04 00:30:34 +0800

    Revised f_realtimebox2gamelog.R for ppd. game.

--- f_realtimebox2gamelog.R
+++ f_realtimebox2gamelog.R
@@ -17,6 +17,7 @@ for (i in 1:length(adddate)){
   # pagetree <- htmlTreeParse(sethtml, useInternalNodes = TRUE, encoding='UTF-8')
   # gameid <- xpathSApply(pagetree,'//h4[@class="vs"]/a',xmlGetAttr,'href')
   # gameid <- substr(gameid,6,nchar(gameid)-1)
+  
   htmlstr <- read_html(paste0("http://sports.yahoo.com/nba/scoreboard/?dateRange=", adddate[i]))
   
   gameid <- htmlstr %>%
@@ -24,6 +25,12 @@ for (i in 1:length(adddate)){
     html_attr("href") %>%
     str_split("/") %>%  #"/nba/golden-state-warriors-toronto-raptors-2016100128/"
     sapply(function(x) x[3])
+  
+  game_status <- htmlstr %>%
+    html_nodes(xpath = "//*[@id='scoreboard-group-2']/div/ul/li/div/div/a/div/div/div/div[1]/div[2]/div/div/div/span") %>%
+    html_text()
+  
+  gameid <- gameid[which(game_status!='Ppd.')]
   
   # gameppd <- xpathSApply(pagetree,'//span[@class="time"]',xmlValue) #game ppd.
   gameppd <- NULL
@@ -41,12 +48,13 @@ for (i in 1:length(adddate)){
     colnames(BoxScroePRALL)[colnames(BoxScroePRALL)=="gamedate"] <- "gdate" #just for html naming
     colnames(BoxScroePRALL)[colnames(BoxScroePRALL)=="oppo"] <- "goppo"
     BoxScroePRALL <- subset(BoxScroePRALL, select = -c(szv, oncourt, livemark)) #allgamelog no need
-
+    
     source('/home/chengil/R/fbasket/f_dbconnect.R')
     dbWriteTable(con, 'allteamlog', BoxScroeTMALL, append = T, row.names = F, allow.keywords = T)
     dbWriteTable(con, 'allgamelog', BoxScroePRALL, append = T, row.names = F, allow.keywords = T)
     dbDisconnect(con)
   }
+  
 }
 
 
