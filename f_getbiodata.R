6537c44db752bcc35cdf22a7c60755f9e5c57bb0
wqchen68 <wqchen68@gmail.com>
2017-10-25 02:01:28 +0800

    revise crawler

--- f_getbiodata.R
+++ f_getbiodata.R
@@ -2,17 +2,19 @@ f_getbiodata = function(fbido){
   library(XML)
   library(RCurl)
   library(RMySQL)
+  library(rvest)
   bio_data <- NULL
   # fbido <- '3704' #normal
   # fbido <- '1250' #no number
   # fbido <- '2874' #no nmber, position
-  sethtml  <- readLines(paste0("http://sports.yahoo.com/nba/players/", fbido),warn=F,encoding = "UTF-8")
+  # fbido <- '5852'
+  sethtml  <- readLines(paste0("https://sports.yahoo.com/nba/players/", fbido),warn=F,encoding = "UTF-8")
   pagetree <- htmlTreeParse(sethtml, useInternalNodes = TRUE, encoding='UTF-8')
   bio_data$fbido  <- fbido
-  bio_data$player <- xpathSApply(pagetree,'//div[@class="player-info"]/h1',xmlValue)
+  bio_data$player <- xpathSApply(pagetree,'//*[@id="Col1-0-Player-Proxy"]/div/div[1]/div[2]/h1/span',xmlValue)
   source('/home/chengil/R/fbasket/t_player2fbid.R')
   bio_data$fbid <- t_player2fbid(bio_data$player)
-  numpos <- xpathSApply(pagetree,'//span[@class="team-info"]',xmlValue) %>% strsplit(",") %>% unlist()
+  numpos <- xpathSApply(pagetree,'//*[@id="Col1-0-Player-Proxy"]/div/div[1]/div[2]/div/div[1]/div',xmlValue) %>% strsplit(",") %>% unlist()
   if (length(numpos)==3){
     bio_data$number   <- numpos[1] %>% strsplit("#") %>% unlist() %>% tail(1)
     bio_data$position <- numpos[2] %>% trimws("left")
@@ -23,28 +25,52 @@ f_getbiodata = function(fbido){
     bio_data$number   <- NULL
     bio_data$position <- NULL
   }
-  bio_data$height  <- xpathSApply(pagetree,'//li[@class="height"]//dd',xmlValue)
-  bio_data$weight  <- xpathSApply(pagetree,'//li[@class="weight"]//dd',xmlValue)
-  bio_data$bornD   <- xpathSApply(pagetree,'//li[@class="born"]//dd',xmlValue)
-  bio_data$bplace  <- xpathSApply(pagetree,'//li[@class="birthplace"]//dd',xmlValue)
-  bio_data$college <- xpathSApply(pagetree,'//li[@class="college"]//dd',xmlValue)
-  draft <- xpathSApply(pagetree,'//li[@class="draft"]//dd',xmlValue)
-  if (draft == "Undrafted") {
-    bio_data$draftY <- NA
-    bio_data$draftR <- NA
-    bio_data$draftP <- NA
-    bio_data$draftT <- NA
+  fbidnp <- as.data.frame(bio_data, stringsAsFactors = F)
+
+  # new version: use rvest
+  setUrl <- paste0('https://sports.yahoo.com/nba/players/', fbido)
+  hwbcd <- setUrl %>% 
+    read_html() %>% 
+    html_nodes(xpath = "//*[@class='Fz(13px) Lh(1.8) Maw(600px)']//*[@class='IbBox Pend(40px) Miw(35%)']") %>% 
+    html_text() %>%
+    strsplit(., split=": ") %>%
+    sapply(.,function(x){
+      df <- as.data.frame(t(x[2]), stringsAsFactors = F)
+      names(df) <- x[1]
+      return(df)
+    })
+  hwbcd <- as.data.frame(hwbcd, stringsAsFactors = F)
+  colnames(hwbcd)[which(colnames(hwbcd) == "Height")] <- 'height'
+  colnames(hwbcd)[which(colnames(hwbcd) == "Weight")] <- 'weight'
+  colnames(hwbcd)[which(colnames(hwbcd) == "Born")] <- 'bornD'
+  colnames(hwbcd)[which(colnames(hwbcd) == "Birth.Place")]  <- 'bplace'
+  colnames(hwbcd)[which(colnames(hwbcd) == "College")]  <- 'college'
+  colnames(hwbcd)[which(colnames(hwbcd) == "Draft")]  <- 'draft'
+
+  # bio_data$height  <- xpathSApply(pagetree,'//*[@id="Col1-0-Player-Proxy"]/div/div[1]/div[2]/div/div[3]/div[1]/span[2]/span',xmlValue)
+  # bio_data$weight  <- xpathSApply(pagetree,'//*[@id="Col1-0-Player-Proxy"]/div/div[1]/div[2]/div/div[3]/div[2]/span[2]/span',xmlValue)
+  # bio_data$bornD   <- xpathSApply(pagetree,'//*[@id="Col1-0-Player-Proxy"]/div/div[1]/div[2]/div/div[3]/div[3]/span[2]/span',xmlValue)
+  # bio_data$bplace  <- xpathSApply(pagetree,'//*[@id="Col1-0-Player-Proxy"]/div/div[1]/div[2]/div/div[3]/div[5]/span[2]',xmlValue)
+  # bio_data$college <- xpathSApply(pagetree,'//*[@id="Col1-0-Player-Proxy"]/div/div[1]/div[2]/div/div[3]/div[4]/span[2]',xmlValue)
+  # draft <- xpathSApply(pagetree,'//*[@id="Col1-0-Player-Proxy"]/div/div[1]/div[2]/div/div[3]/div[6]/span[2]/span',xmlValue)
+  if (hwbcd$draft == "Undrafted") {
+    hwbcd$draftY <- NA
+    hwbcd$draftR <- NA
+    hwbcd$draftP <- NA
+    hwbcd$draftT <- NA
   }else{
-    drafttime <- strsplit(draft, " by the ") %>% unlist %>% head(1) %>% strsplit(" ") %>% unlist
-      bio_data$draftY <- drafttime[1]
-      bio_data$draftR <- drafttime[2] %>% substr(1,1)
-      bio_data$draftP <- stringr::str_sub(drafttime[4], 2, nchar(drafttime[4])-2)
-    bio_data$draftT <- strsplit(draft, " by the ") %>% unlist %>% tail(1)
+    drafttime <- strsplit(hwbcd$draft, " by the ") %>% unlist %>% head(1) %>% strsplit(" ") %>% unlist
+      hwbcd$draftY <- drafttime[1]
+      hwbcd$draftR <- drafttime[2] %>% substr(1,1)
+      hwbcd$draftP <- stringr::str_sub(drafttime[4], 2, nchar(drafttime[4])-2)
+      hwbcd$draftT <- strsplit(hwbcd$draft, " by the ") %>% unlist %>% tail(1)
   }
-  bio_data$fbido2  <- as.numeric(fbido)
-  # return(data.frame(bio_data))
+  hwbcd <- hwbcd[ , names(hwbcd)!='draft']
+  hwbcd$fbido2  <- as.numeric(fbido)
   
-  biodata <- data.frame(bio_data)
+  biodata <- cbind(fbidnp, hwbcd)
+  # biodata <- data.frame(bio_data)
+  
   # source("/home/chengil/R/fbasket/f_dbconnect.R")
   # dbWriteTable(con, 'biodata', biodata, append = T, row.names = F, allow.keywords = T)
   # dbDisconnect(con)
